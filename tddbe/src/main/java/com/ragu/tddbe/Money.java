f763081edc6cad0791cb8df6b5683822126fe5d6
Rain Byun <ragubyun@gmail.com>
2017-02-27 01:56:59 +0900

    init commit

---
+++ tddbe/src/main/java/com/ragu/tddbe/Money.java
@@ -0,0 +1,42 @@
+package com.ragu.tddbe;
+
+public class Money {
+
+    protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return amount == ((Money) obj).amount
+                && currency().equals(((Money) obj).currency);
+    }
+
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    public String currency() {
+        return currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public Money times(int times) {
+        return new Money(amount * times, currency);
+    }
+}
---
+++ tddbe/src/test/java/com/ragu/tddbe/TddbeTest.java
@@ -0,0 +1,32 @@
+package com.ragu.tddbe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TddbeTest {
+
+    @Test
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testEquality() throws Exception {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.dollar(5).equals(Money.franc(5)));
+    }
+
+    @Test
+    public void testCurrency() throws Exception {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+
+    }
+
+}
